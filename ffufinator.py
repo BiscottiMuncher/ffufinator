42f883484e7a48f66625830275111b17eb993903
Evan Metzinger <55550077+BiscottiMuncher@users.noreply.github.com>
2025-01-14 12:30:50 -0800

    Fuffinator 0.2
    Cleaned up output string formatting, changed master address so that it auto generates from machine address, added needed files for dask webgui to work in requirements

--- ffufinator.py
+++ ffufinator.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python3
 
-import os
-import sys
-import subprocess
+import os, datetime, sys, subprocess, socket
 from dask.distributed import Client, as_completed
 
 
 def check_and_start_scheduler():
+    web_address = socket.gethostbyname(socket.gethostname())
     try:
         result = subprocess.run(
             ["pgrep", "dask-scheduler"], 
@@ -14,14 +13,17 @@ def check_and_start_scheduler():
             text=True
         )
         if result.returncode != 0:
-            print("Dask scheduler not running. Starting scheduler...")
-            subprocess.Popen(["dask-scheduler"])  
+            print("Starting Dask scheduler...")
+            subprocess.Popen(["dask-scheduler"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)  
             print("Dask scheduler started.")
+            print(f'Dask webportal started at: {web_address}:8787')
+
         else:
             print("Dask scheduler is already running.")
+            print(f'Dask webportal up at: {web_address}:8787')
+
     except Exception as e:
         print(f"Error checking or starting Dask scheduler: {str(e)}")
-
 
 
 def run_ffuf(directory_chunk, base_url, wordlist_file, ffuf_flags):
@@ -38,8 +40,8 @@ def run_ffuf(directory_chunk, base_url, wordlist_file, ffuf_flags):
         result = subprocess.run(cmd, capture_output=True, text=True, check=True)
         dirty_input = result.stdout.splitlines()
         filtered_lines = [line for line in dirty_input if line.startswith("\x1b[2K")]
-        formatted_lines = [line.replace("\x1b[2K", "").strip() for line in filtered_lines]
-        cleaned_output = "\n".join(formatted_lines)  
+        formatted_lines = [line.replace("\x1b[2K", "").strip() + "\n"  for line in filtered_lines]
+        cleaned_output = "".join(formatted_lines)  
 
     except Exception as e:
         dirty_input = f"Error: {str(e)}"
@@ -48,10 +50,12 @@ def run_ffuf(directory_chunk, base_url, wordlist_file, ffuf_flags):
     
     return cleaned_output
 
-def distribute_work(master_addr, base_url, wordlist, wordlist_file, ffuf_flags):
-    client = Client(f'tcp://{master_addr}:8786') 
+def distribute_work(base_url, wordlist, wordlist_file, ffuf_flags):
+    start_time = datetime.datetime.now()
+    client = Client(f'tcp://{socket.gethostbyname(socket.gethostname())}:8786') 
 
     num_workers = len(client.scheduler_info()['workers'])
+    print(f"Starting task with {num_workers} workers")
     chunk_size = len(wordlist) // num_workers
     wordlist_chunks = [wordlist[i:i + chunk_size] for i in range(0, len(wordlist), chunk_size)]
     
@@ -61,6 +65,9 @@ def distribute_work(master_addr, base_url, wordlist, wordlist_file, ffuf_flags):
     for future in as_completed(futures):
         results.append(future.result())
     
+    end_time = datetime.datetime.now()
+    final_clock = end_time - start_time 
+    print(f'Task completed in: {final_clock.total_seconds()} seconds')
     return results
 
 
@@ -74,22 +81,21 @@ def main():
      ||       ||    ||  ||   ||    ||   ||  ||  .|''||    ||    ||  ||  ||
     .||.     .||.   `|..'|. .||.  .||. .||  ||. `|..||.   `|..' `|..|' .||.
 
-    Usage: ffufinator <master_address> <url + FUZZ> <worslist> <output> [ffuf_flags...]
+    Usage: ffufinator <url + FUZZ> <worslist> <output> [ffuf_flags...]
     """
     if len(sys.argv) < 4:
         print(fancy_text)
         sys.exit(1)
 
-    master_addr =sys.argv[1]
-    base_url = sys.argv[2]  
-    wordlist_file = sys.argv[3]  
-    output_file = sys.argv[4]  
-    ffuf_flags = sys.argv[4:]  
+    base_url = sys.argv[1]  
+    wordlist_file = sys.argv[2]  
+    output_file = sys.argv[3]  
+    ffuf_flags = sys.argv[3:]  
 
     with open(wordlist_file, 'r') as file:
         wordlist = [line.strip() for line in file.readlines()]
     
-    results = distribute_work(master_addr, base_url, wordlist, wordlist_file, ffuf_flags)
+    results = distribute_work(base_url, wordlist, wordlist_file, ffuf_flags)
 
     with open(output_file, 'w') as f:
         for result in results:
